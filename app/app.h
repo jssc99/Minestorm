3b5cac685a37cf229bb336718a34047615f25a7f
j.perrochaud <j.perrochaud@student.isartdigital.com>
2022-11-08 10:04:09 +0100

    modified:   .gitignore 	modified:   app/app.h 	new file:   app/mines.h 	new file:   app/player.h

--- app/app.h
+++ app/app.h
@@ -1,8 +1,8 @@
 #include <stdbool.h>
+#include <math.h>
 #include "Tools/matrix.h"
 
 #define TOLERANCE 0.00001
-#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280
 
 typedef struct App
 {
