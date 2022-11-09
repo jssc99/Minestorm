b2ae46bfc5c7bd076a5934b5b82c75e1a4b0054a
f.becaud <f.becaud@student.isartdigital.com>
2022-11-09 17:31:36 +0100

    Borderless ship and limit speed

--- app/player.c
+++ app/player.c
@@ -1,9 +1,10 @@
 #include "player.h"
 #include <canvas.h>
 
-const bool DEBUG_PLAYER = true;
-const float MAX_SPEED_SHIP = 0.5;
+const bool DEBUG_PLAYER = 1;
+const float MAX_SPEED_SHIP = 10;
 const float ACCELERATION = 0.01;
+const float DECELERATION = 0.99;
 
 // Initialize Player at position(x,y) First time think about setting lives to 3.
 Player player_init(Player p, float x, float y, float size)
@@ -14,10 +15,9 @@ Player player_init(Player p, float x, float y, float size)
                      {size, 0}};
     p.moveLine = p.axis.x;
     p.targetLine = p.moveLine;
-    p.size = size;  
+    p.size = size;
     return p;
 }
-
 
 void draw_circle(Point2 center, unsigned int sides, float radius, float angleOffset, unsigned int color)
 {
@@ -49,12 +49,12 @@ void draw_player(Player p, unsigned int color, float sz)
     {
         Point2 x = addVector2(origin, multVector2(p.axis.x, 2.0));
         Point2 y = addVector2(origin, multVector2(p.axis.y, 2.0));
-        Point2 z = addVector2(origin, multVector2(p.moveLine, 2.0));
-        Point2 dz = addVector2(origin, multVector2(p.moveLine, 2.0+(p.speed*3.0)));
-        cvAddLine(origin.x, origin.y, dz.x, dz.y, CV_COL32(0, 0, 255, 255));  // speed
-        cvAddLine(origin.x, origin.y, z.x, z.y, CV_COL32(255, 0, 255, 255)); //  moveline
-        cvAddLine(origin.x, origin.y, x.x, x.y, CV_COL32(255, 0, 0, 255));  // X axis aka targetline
-        cvAddLine(origin.x, origin.y, y.x, y.y, CV_COL32(0, 255, 0, 255)); //  Y axis
+        Point2 z = addVector2(origin, multVector2(p.inertia, p.size / 2));
+        Point2 dz = addVector2(origin, multVector2(p.moveLine, p.speed/2));
+        cvAddLine(origin.x, origin.y, z.x, z.y, CV_COL32(255, 0, 255, 255)); //  inertia
+        cvAddLine(origin.x, origin.y, dz.x, dz.y, CV_COL32(0, 0, 255, 255)); // speed * moveline
+        cvAddLine(origin.x, origin.y, x.x, x.y, CV_COL32(255, 0, 0, 255));   // X axis aka targetline
+        cvAddLine(origin.x, origin.y, y.x, y.y, CV_COL32(0, 255, 0, 255));   //  Y axis
     }
     // cvAddLine(p.axis.origin.x, p.axis.origin.y, p.axis.origin.x + 30 * p.axis.y.x, p.axis.origin.x + 30 * p.axis.y.y, CV_COL32(0, 0, 255, 255));
     //  cvAddLine(400, 300, 440, 300, CV_COL32(255, 0, 0, 255));
@@ -72,13 +72,21 @@ Player rotate_player(Player p, float angle)
 
 Player update_player(Player p)
 {
+    if(p.axis.origin.x > 1000 - p.size)
+    p.axis.origin.x = p.size;
+    else if(p.axis.origin.x < p.size)
+    p.axis.origin.x = 1000 - p.size;
+    if (p.axis.origin.y > 800 - p.size)
+    p.axis.origin.y = p.size;
+    else if(p.axis.origin.y < p.size)
+    p.axis.origin.y = 800 - p.size;
     p.targetLine = p.axis.x; // multVector2(addVector2(p.axis.x,p.axis.y), 0.5);
-    //Deceleration
-    p.inertia = multVector2(p.inertia,0.99); 
+    // Deceleration
+    p.speed = normVector2(p.inertia);
+    p.inertia = multVector2(p.inertia, DECELERATION);
+    // p.speed *= DECELERATION;
     p.axis = translateAxis2(p.axis, p.inertia);
-    //p.axis = translateAxis2(p.axis, multVector2(p.moveLine, p.speed));
-    //Deceleration
-    p.speed *= 0.99;
+
     return p;
 }
 // Turn the player to the left igIsKeyDown(ImGuiKey_D)
@@ -93,15 +101,19 @@ Player turnleft_player(Player p)
 Player turnright_player(Player p)
 {
     p = rotate_player(p, M_PI / 24);
-    // p.moveLine = p.axis.x;
     return p;
 }
 
 Player accelerate_player(Player p)
 {
     if (p.speed < MAX_SPEED_SHIP)
+    {
         p.speed += ACCELERATION;
+    }
+    else
+        p.inertia = multVector2(p.inertia, MAX_SPEED_SHIP/p.speed);
+    p.inertia = addVector2(p.inertia, multVector2(p.targetLine, ACCELERATION));
     p.moveLine = p.targetLine;
-    p.inertia = addVector2(p.inertia,multVector2(p.moveLine,ACCELERATION));
+
     return p;
 }
