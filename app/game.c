24fc76e6c7a00d36ca121a3917d8639e8105bafc
j.perrochaud <j.perrochaud@student.isartdigital.com>
2022-11-21 12:08:04 +0100

    algo game

---
+++ app/game.c
@@ -0,0 +1,27 @@
+// begin
+// display menu
+// chose game 1p / 2p
+// // if 1p
+// init player pos central
+// enemy array size x
+// minefield enemies
+// // if 2p
+// init player1 pos central left
+// init player2 pos central right
+// enemy array size x
+// minefield enemies
+
+// run
+// display menu
+// update player(s)
+// update mines
+// // if magnet follows p1 if placement array (i) even, else p2
+// until lives p1 p2 == 0 || every mine == DEAD
+// display game over
+// exit / restart
+
+
+// enemies
+// if floating / magnetic dead / minelayer / fireball == dead
+// if fireball_mine / magnet_fire dead = dies and create fireball
+// on death score += points
